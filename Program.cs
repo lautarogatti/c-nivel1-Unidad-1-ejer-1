18c072cc5f6e31feb7a74ba3a96019823a9787c3
lautaro gatti <lautarogatti98@gmail.com>
2024-04-25 01:01:45 -0300

    subida a repo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+
+            Console.WriteLine("Bienvenido a la calculadora de sumas");
+            Console.WriteLine("Por favor inserte el primer numero a sumar:");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Por favor inserte el segundo numero a sumar:");
+            n2 = int.Parse(Console.ReadLine());
+            n3 = n1 + n2;
+            Console.WriteLine("La suma entre " + n1 + " y " + n2 + " tiene como resultado: " + n3);
+            Console.WriteLine("Gracias por utilizar la calculadora de sumas");
+        }
+    }
+}
